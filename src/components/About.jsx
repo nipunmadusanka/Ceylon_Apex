91b3b1fa178256722cc8f2ca1f12ed486250581a
nipun_madusanka <jmnipun@outlook.com>
2023-12-21 01:07:07 +0530

    add

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,12 +6,12 @@ export default function About() {
           <h4 className="leading-none font-medium">About us <span className="inline-block w-16 h-[1px] bg-black ml-4 align-middle"></span></h4>
           <h2 className="text-5xl lg:text-7xl leading-none font-medium">Best creative and modern agency.</h2>
           <h4 className="leading-none font-medium flex gap-[15px] items-center">
-            <a href="" className="text-[#404040] hover:text-[#212529] flex gap-[15px] items-center">
+            {/* <a href="" className="text-[#404040] hover:text-[#212529] flex gap-[15px] items-center">
               Our services 
               <svg viewBox="0 0 9 9" fill="none" className="w-3 h-3">
                 <path d="M8.57107 0.928932C8.57107 0.65279 8.34721 0.428932 8.07107 0.428932L3.57107 0.428932C3.29493 0.428932 3.07107 0.65279 3.07107 0.928932C3.07107 1.20507 3.29493 1.42893 3.57107 1.42893L7.57107 1.42893L7.57107 5.42893C7.57107 5.70507 7.79493 5.92893 8.07107 5.92893C8.34721 5.92893 8.57107 5.70507 8.57107 5.42893L8.57107 0.928932ZM1.35355 8.35355L8.42462 1.28249L7.71751 0.575379L0.646447 7.64645L1.35355 8.35355Z" fill="black"/>
               </svg>
-            </a>
+            </a> */}
           </h4>
         </div>
         <div className="lg:w-1/2 flex flex-col items-start justify-center gap-[30px]">
