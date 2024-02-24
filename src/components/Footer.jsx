927ce0a6b60b74b8e9449ecebcb13af4c571f1c6
nipun_madusanka <jmnipun@outlook.com>
2024-02-24 22:49:29 +0530

    up link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,23 +1,23 @@
 export default function Footer() {
   return (
     <section className="py-24 bg-zinc-800">
-      <div className="lg:w-10/12 w-11/12 mx-auto flex flex-col lg:flex-row gap-[15px] text-white">
+      <div className="lg:w-10/12 w-11/12 mx-auto flex flex-col lg:flex-row text-white gap-5">
         <div className="lg:w-5/12">
           <h1 className="text-4xl font-bold">Ceylon Apex</h1>
-          <p className="mt-[30px] text-white">Step into the extraordinary with Ceylon Apex. We are the pulse of innovation, crafting boundless creativity that transcends the ordinary. Welcome to a world where ideas unfold, and possibilities are limitless. Ceylon Apex – Unleashing the Extraordinary.</p>
+          <p className="mt-[10px] text-white">Step into the extraordinary with Ceylon Apex. We are the pulse of innovation, crafting boundless creativity that transcends the ordinary. Welcome to a world where ideas unfold, and possibilities are limitless. Ceylon Apex – Unleashing the Extraordinary.</p>
         </div>
         <div className="lg:w-3/12 text-white">
-          <h5 className="text-lg uppercase">Contacts</h5>
-          <p className="mt-[30px]">No 1362, Kumburu Idama, 17 canal, Padalangala, Embilipitiya</p>
-          <p className="mt-[30px]">+94719070387 <br />ceylonapex@gmail.com</p>
+          <h5 className="text-lg uppercase font-bold">Contacts</h5>
+          <p className="mt-[10px]">No 1362, Kumburu Idama, 17 canal, Padalangala, Embilipitiya</p>
+          <p className="mt-[10px]">+94719070387 <br />ceylonapex@gmail.com</p>
         </div>
-        <div className="lg:w-2/12 text-white">
+        <div className="lg:w-2/12 text-white font-bold">
           <h5 className="text-lg uppercase">Office</h5>
-          <p className="mt-[30px]">9AM - 5PM <br />(sunday closed)</p>
+          <p className="mt-[10px]">9AM - 5PM <br />(sunday closed)</p>
         </div>
         <div className="lg:w-2/12 text-white">
-          <h5 className="text-lg uppercase">Social</h5>
-          <ul className="mt-[30px] leading-relaxed">
+          <h5 className="text-lg uppercase font-bold">Social</h5>
+          <ul className="mt-[10px] leading-relaxed">
             <li className="hover:text-[#6f6f6f] cursor-pointer">Instagram</li>
             <li className="hover:text-[#6f6f6f] cursor-pointer">Linkedin</li>
             <li className="hover:text-[#6f6f6f] cursor-pointer">Github</li>
--- src/components/ProjGallery.jsx
+++ src/components/ProjGallery.jsx
@@ -2,12 +2,13 @@ export default function ProjGallery() {
   return (
     <div className="lg:w-10/12 w-11/12 flex flex-col lg:grid grid-cols-2 gap-[15px] mx-auto pt-24">
       <div className="overflow-hidden relative scalepointer group">
-        <img
-          src="../img/port1.png"
-          alt="project"
-          className="lg:max-w-full lg:h-auto w-full h-full object-cover group-hover:scale-110 duration-500 ease-in-out"
-        />
         <a href="https://www.youtube.com/watch?v=EMLrkbMyD9g" target="blank">
+          <img
+            src="../img/port1.png"
+            alt="project"
+            className="lg:max-w-full lg:h-auto w-full h-full object-cover group-hover:scale-110 duration-500 ease-in-out"
+          />
+
           <div className="bg-black absolute bottom-[5%] left-[5%] p-3 opacity-0 blur-md group-hover:opacity-100 group-hover:blur-none duration-500">
             <h5 className="text-xl mb-1 text-white">MAIN REEL</h5>
             <p className="uppercase text-xs text-white tracking-wide leading-none">Ceylon Apex</p>
@@ -15,12 +16,13 @@ export default function ProjGallery() {
         </a>
       </div>
       <div className="overflow-hidden relative scalepointer group">
-        <img
-          src="../img/port2.png"
-          alt="project"
-          className="lg:max-w-full lg:h-auto w-full h-80 object-cover group-hover:scale-110 duration-500 ease-in-out"
-        />
         <a href="https://www.youtube.com/watch?v=1rPRV859msQ" target="blank">
+          <img
+            src="../img/port2.png"
+            alt="project"
+            className="lg:max-w-full lg:h-auto w-full h-80 object-cover group-hover:scale-110 duration-500 ease-in-out"
+          />
+
           <div className="bg-black absolute bottom-[5%] left-[5%] p-3 opacity-0 blur-md group-hover:opacity-100 group-hover:blur-none duration-500">
             <h5 className="text-xl mb-1 text-white">Line Art</h5>
             <p className="uppercase text-xs text-white tracking-wide leading-none">Ceylon Apex</p>
@@ -28,12 +30,13 @@ export default function ProjGallery() {
         </a>
       </div>
       <div className="overflow-hidden relative scalepointer group col-start-1 col-end-3">
-        <img
-          src="../img/port5.png"
-          alt="project"
-          className="lg:max-w-full lg:h-auto w-full h-80 object-cover group-hover:scale-110 duration-500 ease-in-out"
-        />
         <a href="https://www.youtube.com/watch?v=zohGyKOU80w" target="blank">
+          <img
+            src="../img/port5.png"
+            alt="project"
+            className="lg:max-w-full lg:h-auto w-full h-80 object-cover group-hover:scale-110 duration-500 ease-in-out"
+          />
+
           <div className="bg-black absolute bottom-[5%] left-[2.5%] p-3 opacity-0 blur-md group-hover:opacity-100 group-hover:blur-none duration-500">
             <h5 className="text-xl mb-1 text-white">Landing Page</h5>
             <p className="uppercase text-xs text-[#626161] tracking-wide leading-none">Ceylon Apex</p>
@@ -41,14 +44,57 @@ export default function ProjGallery() {
         </a>
       </div>
       <div className="overflow-hidden relative scalepointer group col-start-1 col-end-3">
-        <img
-          src="../img/port6.png"
-          alt="project"
-          className="lg:max-w-full lg:h-auto w-full h-80 object-cover group-hover:scale-110 duration-500 ease-in-out"
-        />
         <a href="https://www.youtube.com/watch?v=vBmTqIf1gDg" target="blank">
+          <img
+            src="../img/port6.png"
+            alt="project"
+            className="lg:max-w-full lg:h-auto w-full h-80 object-cover group-hover:scale-110 duration-500 ease-in-out"
+          />
+
           <div className="bg-black absolute bottom-[5%] left-[2.5%] p-3 opacity-0 blur-md group-hover:opacity-100 group-hover:blur-none duration-500">
             <h5 className="text-xl mb-1 text-white">Character Art</h5>
+            <p className="uppercase text-xs text-[#626161] tracking-wide leading-none">Ceylon Apex</p>
+          </div>
+        </a>
+      </div>
+      <div className="overflow-hidden relative scalepointer group col-start-1 col-end-3">
+        <a href="https://www.youtube.com/watch?v=T5QNEk9_KR4" target="blank">
+          <img
+            src="../img/port8.png"
+            alt="project"
+            className="lg:max-w-full lg:h-auto w-full h-80 object-cover group-hover:scale-110 duration-500 ease-in-out"
+          />
+
+          <div className="bg-black absolute bottom-[5%] left-[2.5%] p-3 opacity-0 blur-md group-hover:opacity-100 group-hover:blur-none duration-500">
+            <h5 className="text-xl mb-1 text-white">Explanation Video</h5>
+            <p className="uppercase text-xs text-[#626161] tracking-wide leading-none">Ceylon Apex</p>
+          </div>
+        </a>
+      </div>
+      <div className="overflow-hidden relative scalepointer group col-start-1 col-end-3">
+        <a href="https://www.youtube.com/watch?v=_SSOUeovTfU" target="blank">
+          <img
+            src="../img/port7.png"
+            alt="project"
+            className="lg:max-w-full lg:h-auto w-full h-80 object-cover group-hover:scale-110 duration-500 ease-in-out"
+          />
+
+          <div className="bg-black absolute bottom-[5%] left-[2.5%] p-3 opacity-0 blur-md group-hover:opacity-100 group-hover:blur-none duration-500">
+            <h5 className="text-xl mb-1 text-white">Life Story</h5>
+            <p className="uppercase text-xs text-[#626161] tracking-wide leading-none">Ceylon Apex</p>
+          </div>
+        </a>
+      </div>
+      <div className="overflow-hidden relative scalepointer group col-start-1 col-end-3">
+        <a href="https://www.youtube.com/watch?v=Oacoyp9MU1o" target="blank">
+          <img
+            src="../img/port9.png"
+            alt="project"
+            className="lg:max-w-full lg:h-auto w-full h-80 object-cover group-hover:scale-110 duration-500 ease-in-out"
+          />
+
+          <div className="bg-black absolute bottom-[5%] left-[2.5%] p-3 opacity-0 blur-md group-hover:opacity-100 group-hover:blur-none duration-500">
+            <h5 className="text-xl mb-1 text-white">Short Film</h5>
             <p className="uppercase text-xs text-[#626161] tracking-wide leading-none">Ceylon Apex</p>
           </div>
         </a>
@@ -65,12 +111,12 @@ export default function ProjGallery() {
         </div>
       </div>
       <div className="overflow-hidden relative scalepointer group">
-        <img
-          src="../img/port3.png"
-          alt="project"
-          className="lg:max-w-full lg:h-auto w-full h-80 object-cover group-hover:scale-110 duration-500 ease-in-out"
-        />
         <a href="https://www.youtube.com/watch?v=NEEUXJrvO64" target="blank">
+          <img
+            src="../img/port3.png"
+            alt="project"
+            className="lg:max-w-full lg:h-auto w-full h-80 object-cover group-hover:scale-110 duration-500 ease-in-out"
+          />
           <div className="bg-black absolute bottom-[5%] left-[5%] p-3 opacity-0 blur-md group-hover:opacity-100 group-hover:blur-none duration-500">
             <h5 className="text-xl mb-1 text-white">Character Movement</h5>
             <p className="uppercase text-xs text-white tracking-wide leading-none">Ceylon Apex</p>
